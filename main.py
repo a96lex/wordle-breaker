dbd9467102be397898fc70e8d7c82edfae9f58f5
a96lex <a96lex@gmail.com>
2022-01-24 18:57:07 +0100

    change more var names

--- main.py
+++ main.py
@@ -28,77 +28,76 @@ class WordleSolver:
             self.curr_choice = random.choice(self.all_words_list)
 
         else:
-            potential_next_answer = self.get_final_answers()
-            self.possible_answers = potential_next_answer
-            if len(potential_next_answer) == 1:
-                print(f'Answer found! introduce "{potential_next_answer[0]}" to win')
+            potential_next_answers = self.get_possible_final_answer()
+            self.possible_answers = potential_next_answers
+            if len(potential_next_answers) == 1:
+                print(f'Answer found! introduce "{potential_next_answers[0]}" to win')
                 exit()
-            print(f"\nThere are {len(potential_next_answer)} potential answers")
-            if len(potential_next_answer) > MAX_TURNS - self.turn:
+            print(f"\nThere are {len(potential_next_answers)} potential answers")
+            if len(potential_next_answers) > MAX_TURNS - self.turn:
                 choose_another = self.get_missing_letters()
                 if len(choose_another) > 0:
-                    potential_next_answer = choose_another
-                else:
+                    potential_next_answers = choose_another
                     print(
                         """
 since there are more potential answers than turns remaining,
-introduce a random word containing none of the already guessed letters"""
+introduce a random word containing none of the already guessed letters
+(or enter "p" to see all possible answers)"""
                     )
-            self.curr_choice = random.choice(potential_next_answer)
-            print("introduce this randomly selected word from the list")
+            self.curr_choice = random.choice(potential_next_answers)
+            print("Introduce this randomly selected word from the list")
 
         self.turn += 1
 
         print(f"\t{self.curr_choice}")
         return self.curr_choice
 
-    def get_final_answers(self):
-        potential_next_answer = []
-        for d in self.all_words_list:
-            maybe = True
-            for k, v in self.correct_with_position.items():
-                if not d[k] == v:
-                    maybe = False
+    def get_possible_final_answer(self):
+        potential_next_answers = []
+        for word in self.all_words_list:
+            is_possible_answer = True
+            for pos, letter in self.correct_with_position.items():
+                if not word[pos] == letter:
+                    is_possible_answer = False
 
-            for k, v in self.present_but_incorrect_position_dict.items():
-                for l in v:
-                    if d[k] == l:
-                        maybe = False
+            for (
+                pos,
+                incorrect_pos_letters,
+            ) in self.present_but_incorrect_position_dict.items():
+                for letter in incorrect_pos_letters:
+                    if word[pos] == letter:
+                        is_possible_answer = False
 
-            for c in self.present_but_incorrect_position:
-                if not c in d:
-                    maybe = False
+            for letter in self.present_but_incorrect_position:
+                if not letter in word:
+                    is_possible_answer = False
 
-            for c in self.not_present:
-                if c in d:
-                    maybe = False
+            for letter in self.not_present:
+                if letter in word:
+                    is_possible_answer = False
 
-            if maybe:
-                potential_next_answer.append(d)
+            if is_possible_answer:
+                potential_next_answers.append(word)
 
-        return potential_next_answer
+        return potential_next_answers
 
     def get_missing_letters(self):
 
-        potential_next_answer = []
-        for d in self.all_words_list:
-            maybe = True
-            for c in self.present_but_incorrect_position:
-                if c in d:
-                    maybe = False
+        words_with_unseen_letters = []
+        for word in self.all_words_list:
+            contains_seen_letters = False
+            for letter in [
+                *self.present_but_incorrect_position,
+                *self.not_present,
+                *self.correct_with_position.values(),
+            ]:
+                if letter in word:
+                    contains_seen_letters = True
 
-            for c in self.not_present:
-                if c in d:
-                    maybe = False
+            if not contains_seen_letters:
+                words_with_unseen_letters.append(word)
 
-            for c in self.correct_with_position.values():
-                if c in d:
-                    maybe = False
-
-            if maybe:
-                potential_next_answer.append(d)
-
-        return potential_next_answer
+        return words_with_unseen_letters
 
     def handle_input(self, pattern: str):
         """
